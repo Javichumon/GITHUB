ea5418eb741703bc87a99104b58692e8cc2e053d
Javichumon <javierperaltagarrido@gmail.com>
2023-10-17 17:35:26 +0200

    ok

---
+++ Servidor/Unidad 2/C#/PruebaPractica/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChessAPI.Model 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Piece whiteRook= new Rook(Piece.ColorEnum.WHITE);
+            //Console.WriteLine($"White Rook Code: {whiteRook.GetCode()}")
+            //Console.WriteLine($"White Rook Score: {whiteRook.GetScore()}")
+            Console.WriteLine("Hola");
+        }   
+    }
+}
